0e428ab9b56e5c4ae1d5dbb8973062784a224887
Mostafa Mohiuddin Jalal <protikcodes@gmail.com>
2025-12-23 16:10:43 +1100

    LangGraph init-latest

--- with_structured_output.py
+++ with_structured_output.py
@@ -2,15 +2,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
 ##################################
@@ -29,43 +25,29 @@ tools = [TavilySearch()]
 # task 2: define the LLM
 ##################################
 llm = ChatOpenAI(model="gpt-4o-mini")
-structured_llm = llm.with_structured_output(AgentResponse)
-react_prompt_with_format_instruction = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=[
-        "input",
-        "agent_scratchpad",
-        "tools",
-        "tool_names",
-    ],
-).partial(format_instructions="")
-
-agent = create_react_agent(
-    llm=llm,
+agent = create_agent(
+    model=llm,
     tools=tools,
-    prompt=react_prompt_with_format_instruction,
+    response_format=ToolStrategy(AgentResponse),
+    system_prompt=(
+        "You are a helpful assistant. Use tools when needed and return sources with URLs."
+    ),
 )
-
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True,
-)
-extract_output = RunnableLambda(lambda x: x["output"])
-
-chain = agent_executor | extract_output | structured_llm
 
 
 def main():
     print("Hello from ReAct Agent Depth one")
-    result = chain.invoke(
-        input={
-            "input": [
-                "Search 3 job posting in AI/ML engineering in greater sydney area in LinkedIn."
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Search 3 job posting in AI/ML engineering in greater sydney area in LinkedIn.",
+                }
             ]
         }
     )
-    print("Final Result:", result)
+    print("Final Result:", result["structured_response"])
 
 
 if __name__ == "__main__":
